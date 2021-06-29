016b5716f324113052d8afeb18c8627ee98019fc
Initializer <33171453+XInitialize@users.noreply.github.com>
2021-06-29 11:55:00 +0800

    Create collectionspp.py

---
+++ collectionspp.py
@@ -0,0 +1,9 @@
+def reduce_namedtuples_for_httpRequest(tpdict):
+    kvfinal = {}
+    for key in tpdict._fields:
+        _val = getattr(tpdict, (key))
+        if isinstance(_val, list):
+            kvfinal.update({key, _val.pop()})
+        else:
+            kvfinal.update({key, _val})
+    return tpdict._replace(**kvfinal)
