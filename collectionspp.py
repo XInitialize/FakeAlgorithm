80cc46d968bc68bf6da95e1433c76e7d56602b41
Initializer <33171453+XInitialize@users.noreply.github.com>
2021-07-16 15:00:21 +0800

    Update collectionspp.py

--- collectionspp.py
+++ collectionspp.py
@@ -3,7 +3,10 @@ def reduce_namedtuples_for_httpRequest(tpdict):
     for key in tpdict._fields:
         _val = getattr(tpdict, (key))
         if isinstance(_val, list):
-            kvfinal.update({key: _val.pop()})
+            if len(_val)==1:
+                kvfinal.update({key: _val.pop()})
+            else:
+                kvfinal.update({key: _val})
         else:
             kvfinal.update({key: _val})
     return tpdict._replace(**kvfinal)
